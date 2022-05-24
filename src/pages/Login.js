eafaa683fcfffe38f3e3d9ee0b3f6cb83d9842d2
nayr <ryan567.pr@gmail.com>
2022-05-24 19:17:11 -0300

    Requisito 01 tela de login concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import logo from '../trivia.png';
 
-class Login extends Component {
+export default class Login extends Component {
   constructor() {
     super();
 
@@ -8,20 +9,70 @@ class Login extends Component {
       name: '',
       email: '',
       disabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.disabledBtn());
+  }
+
+  disabledBtn = () => {
+    const { name, email } = this.state;
+    if (name === '' || email === '') {
+      this.setState({
+        disabled: true,
+      });
+    } else {
+      this.setState({
+        disabled: false,
+      });
     }
   }
 
   render() {
+    const { name, email, disabled } = this.state;
     return (
-      <main>
-        <label htmlFor='inputName'>
-          <input
-            name='inputName'
-            type='text'
-            data-testid='input-player-name'
-          />
-        </label>
-      </main>
-    )
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={ logo } className="App-logo" alt="logo" />
+          </header>
+        </div>
+        <main>
+          <label htmlFor="inputName">
+            <input
+              name="name"
+              id="inputName"
+              type="text"
+              data-testid="input-player-name"
+              value={ name }
+              onChange={ this.onInputChange }
+            />
+          </label>
+
+          <label htmlFor="inputEmail">
+            <input
+              name="email"
+              id="inputEmail"
+              type="text"
+              data-testid="input-gravatar-email"
+              value={ email }
+              onChange={ this.onInputChange }
+            />
+          </label>
+
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ disabled }
+          >
+            Play
+          </button>
+        </main>
+      </div>
+    );
   }
 }

c149e04a26ccc2ce3c7a332b3d19f67c8af8de81
vicdev <vtavares.tur@gmail.com>
2022-05-24 17:57:19 -0300

    req1 - teste de implementação na branch do requisito em questão

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      disabled: true,
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <label htmlFor='inputName'>
+          <input
+            name='inputName'
+            type='text'
+            data-testid='input-player-name'
+          />
+        </label>
+      </main>
+    )
+  }
 }

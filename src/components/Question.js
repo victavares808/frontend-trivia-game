636c741b301212473f00c74db6bf847d289ef87c
RabeloGuedes <krepscredian@gmail.com>
2022-05-26 18:01:52 -0300

    Tentamos.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import Loading from './Loading';
 
 class Question extends Component {
   state = {
@@ -17,30 +18,50 @@ class Question extends Component {
     }
   }
 
+  // Source: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm;
+  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
   render() {
     const { questionNumber } = this.state;
     const { questions } = this.props;
     const answers = questions[questionNumber].incorrect_answers;
     answers.push(questions[questionNumber].correct_answer);
-    console.log(answers);
+    this.shuffleArray(answers);
     return (
       <div>
-        <h1>
-          {questions[questionNumber].question}
-        </h1>
-        {answers.map((answer) => (
-          <div key={ Math.random() }>
-            <button type="button">
-              {answer}
-            </button>
+        <div>
+          <h1 data-testid="question-text">
+            {questions[questionNumber].question}
+          </h1>
+          <h3 data-testid="question-category">
+            {questions[questionNumber].category}
+          </h3>
+          <div data-testid="answer-options">
+            {answers.map((answer, index) => (
+              <button
+                key={ index }
+                data-testid={
+                  questions[questionNumber].correct_answer === answer
+                    ? 'correct-answer' : `wrong-answer-${index}`
+                }
+                type="button"
+              >
+                {answer}
+              </button>
+            ))}
           </div>
-        ))}
-        <button
-          type="button"
-          onClick={ this.nextQuestion }
-        >
-          Próximo
-        </button>
+          <button
+            type="button"
+            onClick={ this.nextQuestion }
+          >
+            Próximo
+          </button>
+        </div>
       </div>
     );
   }
@@ -50,10 +71,4 @@ Question.propTypes = {
   questions: PropTypes.arrayOf(),
 }.isRequired;
 
-const mapStateToProps = (state) => ({
-  quests: state.quizReducer.quests,
-});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Question);
+export default connect()(Question);
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -15,29 +15,37 @@ class Game extends Component {
   }
 
   async componentDidMount() {
-    const { getQuiz, history } = this.props;
-    const token = localStorage.getItem('token');
-    const quiz = await fetchQuiz(token);
-    if (quiz.response_code === invalidTokenId) {
-      localStorage.removeItem('token');
-      history.push('/');
-    } else {
-      getQuiz(quiz);
-      this.setState({ loading: false });
-    }
+    this.quizRequest();
   }
 
-  render() {
-    const { quests } = this.props;
-    const { loading } = this.state;
-    return (loading ? <Loading /> : (
-      <div>
-        <Header />
-        <Question questions={ quests } />
-      </div>
-    )
-    );
-  }
+    quizRequest = async () => {
+      this.setState({
+        loading: true,
+      });
+      const { getQuiz, history } = this.props;
+      const token = localStorage.getItem('token');
+      const quiz = await fetchQuiz(token);
+      if (quiz.response_code === invalidTokenId) {
+        localStorage.removeItem('token');
+        history.push('/');
+      } else {
+        getQuiz(quiz);
+        this.setState({
+          loading: false,
+        });
+      }
+    }
+
+    render() {
+      const { quests } = this.props;
+      const { loading } = this.state;
+      return (
+        <div>
+          <Header />
+          { loading ? <Loading /> : (<Question questions={ quests } />)}
+        </div>
+      );
+    }
 }
 
 Game.propTypes = {

a5ce013e330b5205719be451b9d3769a17bd8cd3
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-27 20:03:34 -0300

    Adiciona rota para a página de Ranking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import logo from './trivia.png';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
         <Route exact path="/feedback" component={ Feedbacḱ } />
         <Route exact path="/game" component={ Game } />
         <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );

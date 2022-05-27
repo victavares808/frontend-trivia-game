8a9cc23854603a23a92caf5803e35f551d2bbcd2
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-27 18:06:30 -0300

    Adiciona função de mensagem de feedback, requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  feedbackMessage = () => {
+    const { assertions } = this.props;
+    const numberThree = 3;
+    if (assertions < numberThree) {
+      return <h1 data-testid="feedback-text">Could be better...</h1>;
+    }
+    return <h1 data-testid="feedback-text">Well Done!</h1>;
+  }
+
   render() {
+    const { feedbackMessage } = this;
     return (
       <div>
         <Header />
         <div>
-          <h1 data-testid="feedback-text">
-            Feedback
-          </h1>
+          { feedbackMessage() }
         </div>
       </div>
     );
   }
 }
 
-export default connect()(Feedback);
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);

2e2192f46df646ff77497d2af9e6a964f597223c
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-05-28 01:45:00 -0300

    Adiciona botão na página Ranking que redireciona para Login, requisito  18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../components/Button';
 
 class Ranking extends Component {
+  goHomeRedirect = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <>
+        <div data-testid="ranking-title">
+          Ranking
+        </div>
+        <Button
+          name="Go Home"
+          testId="btn-go-home"
+          onClick={ this.goHomeRedirect }
+        />
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func }),
+}.isRequired;
 
 export default Ranking;

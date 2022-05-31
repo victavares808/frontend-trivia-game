100b8926ea84981f9e8c32a789f381d554ce13e4
vicdev <vtavares.tur@gmail.com>
2022-05-31 15:53:33 -0300

    implementação de testes para a tela de Feedback para requisito 17

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Quiz from './pages/Quiz';
+import Quiz from './pages/Game';
 import Settings from './pages/Settings';
 
 const Routes = () => (
